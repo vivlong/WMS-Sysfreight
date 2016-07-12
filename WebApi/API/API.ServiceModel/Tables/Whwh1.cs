c50b1c4916c5ed31607603a2fac239ed8e0804c4
vivlong <vivlonglz@gmail.com>
2016-07-12 14:12:19 +0800

    Update Impm1_Enquiry.cs

---
+++ WebApi/API/API.ServiceModel/Tables/Whwh1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebApi.ServiceModel.Tables
+{
+    public class Whwh1
+    {
+								public string WarehouseCode { get; set; }
+								public string WarehouseName { get; set; }
+    }
+}
