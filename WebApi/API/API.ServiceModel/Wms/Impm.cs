2221cbee411e4ab306c30e85af84b8b55d9ec299
vivlong <vivlonglz@gmail.com>
2016-07-17 22:30:44 +0800

    Update Impm

--- WebApi/API/API.ServiceModel/Wms/Impm.cs
+++ WebApi/API/API.ServiceModel/Wms/Impm.cs
@@ -80,17 +80,17 @@ namespace WebApi.ServiceModel.Wms
 												{
 																using (var db = DbConnectionFactory.OpenDbConnection("WMS"))
 																{
-																				string strSql = "Select TrxNo, BatchLineItemNo, IsNull(BatchNo,'') AS name, IsNull(ProductCode,'') AS ProductCode," +
+																				string strSql = "Select Impm1.TrxNo, Impm1.BatchLineItemNo, IsNull(BatchNo,'') AS name, IsNull(ProductCode,'') AS ProductCode," +
 																								"IsNull(ProductName,'') AS ProductName, IsNull(GoodsReceiveorIssueNo,'') AS GoodsReceiveorIssueNo, IsNull(UserDefine1,'') AS UserDefine1," +
-																								"(CASE Impm1.DimensionFlag When '1' THEN Impm1.PackingQty When '2' THEN Impm1.WholeQty ELSE Impm1.LooseQty END) AS Qty, " +
-																								"'' AS FromToStoreNo, 0 AS QtyBal, 0 AS ScanQty " +
-																								"From Impm1 " +
-																								"Where WarehouseCode='" + request.WarehouseCode + "' And StoreNo='" + request.StoreNo + "'";
+																								"b.QtyBal, '' AS FromToStoreNo, 0 AS ScanQty " +
+																								"From Impm1 Join (Select (Select top 1 Imit1.StatusCode from imit1 Where imit1.GoodsTransferNoteNo = a.GoodsReceiveorIssueNo) AS ImitStatus, a.TrxNo, " +
+																								"(CASE a.DimensionFlag When '1' THEN a.BalancePackingQty When '2' THEN a.BalanceWholeQty ELSE a.BalanceLooseQty END) AS QtyBal From Impm1 a ) b on b.TrxNo = impm1.TrxNo " +
+																								"Where WarehouseCode='" + request.WarehouseCode + "' And StoreNo='" + request.StoreNo + "' And (b.ImitStatus = 'EXE' or ImitStatus is null) And b.QtyBal>0";
 																				Results = db.Select<Impm1_Transfer>(strSql);
 																				for (int i = 0; i < Results.Count; i++)
 																				{
 																								string BatchNo = Results[i].name;
-																								Impm1_Transfer impm1 = Results[i];																								
+																								Impm1_Transfer impm1 = Results[i];
 																								bool blnExistBatchNo = false;
 																								foreach (Impm1_Transfer_Tree ResultTree in ResultTrees)
 																								{
@@ -98,7 +98,7 @@ namespace WebApi.ServiceModel.Wms
 																												{
 																																blnExistBatchNo = true;
 																																ResultTree.tree.Add(impm1);
-																												}																											
+																												}
 																								}
 																								if (!blnExistBatchNo)
 																								{
@@ -107,7 +107,7 @@ namespace WebApi.ServiceModel.Wms
 																												impm1_tree.tree = new List<Impm1_Transfer>();
 																												impm1_tree.tree.Add(impm1);
 																												ResultTrees.Add(impm1_tree);
-																								}
+																								}																					
 																				}
 																}
 												}
